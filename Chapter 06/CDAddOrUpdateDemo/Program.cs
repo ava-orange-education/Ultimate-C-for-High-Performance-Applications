e8b3e691a77b3974d9517f1fffac979df1ae25d7
Jeff McNamara <mcjeff371@hotmail.com>
2025-07-01 14:26:47 -0600

    Added AddOrUpdateDemo in Chapter 6

---
+++ Chapter 06/CDAddOrUpdateDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+
+var dictionary = new ConcurrentDictionary<string, int>();
+
+//Add new entry
+int addedValue = dictionary.AddOrUpdate("apple", 1, 
+    (key, oldvalue) => oldvalue + 1);
+Console.WriteLine($"apple: {addedValue}");
+
+//Update existing entry
+int updatedValue = dictionary.AddOrUpdate("apple", 1, 
+    (key, oldvalue) => oldvalue + 1);
+Console.WriteLine($"apple: {updatedValue}");
